e94efbd17f5aba53128fd3df6f4b09f89b0914a4
guijt <guijt@zjrcu.com>
2021-01-25 15:53:46 +0800

    creat branch:dev-gjt

--- src/demo.java
+++ src/demo.java
@@ -2,6 +2,7 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("mod by gjt");
+        System.out.println("creat new branch: dev-gjt");
 
     }
 }
